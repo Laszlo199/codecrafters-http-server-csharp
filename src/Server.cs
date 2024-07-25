7a37c9a12ef3ecd51aba491f6364a9b09986a152
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-07-25 10:38:30 +0200

    Test

--- src/Server.cs
+++ src/Server.cs
@@ -8,5 +8,26 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var send = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-server.AcceptSocket().Send(send); // wait for client
+var socket = server.AcceptSocket();
+var send200 = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+var send404 = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+//ToDo Read the data
+//Parse the HTTP request to extract
+//Send a 200 OK response for the path / and a 404 Not Found response for any other path.
+byte[] data = new byte[1000];
+int receivedData = socket.Receive(data);
+string stringData = Encoding.UTF8.GetString(data, 0, receivedData);
+string[] requestLines = stringData.Split(new[] { "\r\n" }, StringSplitOptions.None);
+string requestLine = requestLines[0];
+string[] requestLineTokens = requestLine.Split(' ');
+string uri = requestLineTokens[1];
+
+if(uri == "/")
+{
+    socket.Send(send200);
+}
+else
+{
+    socket.Send(send404);
+}
+
