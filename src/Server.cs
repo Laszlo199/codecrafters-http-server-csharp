4837e634d0e63ee87580d12551f2e40380918140
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-07-26 14:47:30 +0200

    Test

--- src/Server.cs
+++ src/Server.cs
@@ -21,15 +21,16 @@ string stringData = Encoding.UTF8.GetString(data, 0, receivedData);
 string[] requestLines = stringData.Split(new[] { "\r\n" }, StringSplitOptions.None);
 string requestLine = requestLines[0];
 string[] requestLineTokens = requestLine.Split(' ');
-string uri = requestLineTokens[1];
+string userAgentToken = requestLines[2];
+var uri = requestLineTokens[1];  
 
 var message = requestLineTokens[1].Replace("/echo/",string.Empty);
 int lenght = message.Length;
-var respons200 = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {lenght}\r\n\r\n{message}"; 
+var respons200 = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {lenght}\r\n\r\n{userAgentToken}"; 
 var send200 = Encoding.UTF8.GetBytes(respons200);
 var send404 = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
 
-if( uri.Contains("/echo/" ) || uri.Length == 1)
+if( uri.Contains("/echo/" ) || uri.Length > 1)
 {
     socket.Send(send200);
 }
