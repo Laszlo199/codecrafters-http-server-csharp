6ed38b4335d2d6b6107705441e27b6539b955d76
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-07-26 13:53:46 +0200

    Test

--- src/Server.cs
+++ src/Server.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.VisualBasic;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -21,19 +22,14 @@ string[] requestLines = stringData.Split(new[] { "\r\n" }, StringSplitOptions.No
 string requestLine = requestLines[0];
 string[] requestLineTokens = requestLine.Split(' ');
 string uri = requestLineTokens[1];
-var message = "";
 
-if (requestLineTokens[1].Contains("/echo/"));
-{
-    message = requestLineTokens[1].Remove(6);
-}
-
-var lenght = message.Length.ToString();
-var respons200 = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {lenght}\r\n\r\n{message}";
+var message = requestLineTokens[1].Replace("/echo/",string.Empty);
+int lenght = message.Length;
+var respons200 = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {lenght}\r\n\r\n{message}"; 
 var send200 = Encoding.UTF8.GetBytes(respons200);
 var send404 = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
 
-if(uri =="/")
+if(uri.Length > 0)
 {
     socket.Send(send200);
 }
@@ -41,4 +37,3 @@ else
 {
     socket.Send(send404);
 }
-
